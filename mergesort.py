84b076fff3da25797d7dd4bb17df2733ed226d25
Michał Grablowski <michalgra25@gmail.com>
2022-03-26 14:04:19 +0100

    mergesort

---
+++ mergesort.py
@@ -0,0 +1,37 @@
+array = [200, 100, 300, 1230, -312, 321, 21, 3, 1, 8 , 9, -32, 9, 1, 0]
+
+
+# merge sort
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k+=1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+    return arr
+
+print("merge sort")
+print(array)
+print(mergeSort(array))
